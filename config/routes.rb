92d9ce12e6828a28eb7efa4e5cb55bee1c600780
Sunny Wong <swong194@gmail.com>
2018-02-13 12:08:39 -0500

    refactored routes to render json and templates under api namespace

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'home#root'
-  resources :users, only: [:create, :index]
-  resource :session, only: [:create, :destroy]
-  resources :classlist, only: [:index]
+  namespace :api, default: { format: :json } do
+    resources :users, only: [:create, :index]
+    resource :session, only: [:create, :destroy]
+    resources :classlist, only: [:index]
+  end
 end
