aa63ac8d8f961064c5a0b21bbcdf0b51da4f271a
Sunny Wong <swong194@gmail.com>
2018-02-13 12:04:57 -0500

    refactored home controller to render root template

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
 
-  def index
-  end 
+  def root
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
-  resources :home, only: [:index], :path => "/"
+
+  root 'home#root'
   resources :users, only: [:create, :index]
   resource :session, only: [:create, :destroy]
   resources :classlist, only: [:index]
